8288bb44ae11525aa055e11aac86e313b35be534
Ayoubouljihate <80477398+Ayoubouljihate@users.noreply.github.com>
2021-03-14 13:43:53 +0100

    Add files via upload

---
+++ Projet créateur de mot de passe.py
@@ -0,0 +1,12 @@
+import random
+# demander à l'utilisateur de saisir la longueur du mot passe avec 'input'
+nombres_de_caractères=int(input('donner le nombre de caractères du mot de passe'))
+# la liste des lettres, majuscules et nombre pour le mot de passe
+liste_majuscules_lettres_nombre='abcdefghijklmnopqrstuvwxyza0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#cette fonction permet de selectionner des caractéres au hasard dans l'ensemble 'liste_majuscules_lettres_nombre'
+print(random.sample(liste_majuscules_lettres_nombre,nombres_de_caractères))
+
+
+
+
+#objectif(plus tard): regrouper les  caractéres selectionnés aléatoirement en un seul mot et générer un login pour plus tard.
