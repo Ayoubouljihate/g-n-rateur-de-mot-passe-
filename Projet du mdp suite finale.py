d6d2b7b52a44360fe349bf316081c513cb24b93d
Ayoubouljihate <80477398+Ayoubouljihate@users.noreply.github.com>
2021-03-16 22:11:53 +0100

    Add files via upload
    J'ai rajouter des espaces au lignes des print pour que lorsque on tape le numéro choisi il y'a  un espace entre le nombre et le texte

--- Projet du mdp suite finale.py
+++ Projet du mdp suite finale.py
@@ -1,6 +1,6 @@
 import random
 # demander à l'utilisateur de saisir la longueur du mot passe avec 'input'
-nombres_de_caractères=int(input('donner nombre de caractères du mdp'))
+nombres_de_caractères=int(input('donner nombre de caractères du mdp '))
 # la liste des lettres, majuscules et nombre pour le mot de passe
 liste_majuscules_lettres_nombre='abcdefghijklmnopqrstuvwxyza0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # regrouper les caractères selectionnés en un seul bloc
@@ -9,7 +9,7 @@ print(motdepasse)
 
 
 # demander à l'utilisateur de saisir le nombre de caractères du login avec  'input'
-nombres_de_caractère=int(input('donner nombre de caractères du login'))
+nombres_de_caractère=int(input('donner nombre de caractères du login '))
 # la liste des lettres, majuscules et nombre pour le mot de passe
 liste_majuscules_lettres_nombres='abcdefghijklmnopqrstuvwxyza0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # regrouper les caractères selectionnés en un seul bloc
