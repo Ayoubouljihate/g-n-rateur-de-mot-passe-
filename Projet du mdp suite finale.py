89a523b9afd247936a1f5ef538f455391957976e
Ayoubouljihate <80477398+Ayoubouljihate@users.noreply.github.com>
2021-03-25 07:28:00 +0100

    Add files via upload

--- Projet du mdp suite finale.py
+++ Projet du mdp suite finale.py
@@ -1,19 +1,27 @@
 import random
 # demander à l'utilisateur de saisir la longueur du mot passe avec 'input'
+
 nombres_de_caractères=int(input('donner nombre de caractères du mdp '))
 # la liste des lettres, majuscules et nombre pour le mot de passe
+
 liste_majuscules_lettres_nombre='abcdefghijklmnopqrstuvwxyza0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # regrouper les caractères selectionnés en un seul bloc
+
 motdepasse="".join(random.sample(liste_majuscules_lettres_nombre,nombres_de_caractères))
+
 print(motdepasse)
 
 
 # demander à l'utilisateur de saisir le nombre de caractères du login avec  'input'
+
 nombres_de_caractère=int(input('donner nombre caractères du login '))
 # la liste des lettres, majuscules et nombre pour le mot de passe
+
 liste_majuscules_lettres_nombres='abcdefghijklmnopqrstuvwxyza0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # regrouper les caractères selectionnés en un seul bloc
+
 login="".join(random.sample(liste_majuscules_lettres_nombres,nombres_de_caractère))
+
 print(login)
 
 
